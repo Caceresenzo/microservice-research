2ab533dbbffe517d544b869789c4edcdfd3bea1d
Caceresenzo <caceresenzo1502@gmail.com>
2022-08-05 00:50:03 +0200

    fix(eureka): better server and clients configuration

--- index/src/main/java/caceresenzo/hello/index/IndexController.java
+++ index/src/main/java/caceresenzo/hello/index/IndexController.java
@@ -1,5 +1,7 @@
 package caceresenzo.hello.index;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.servlet.context.ServletWebServerApplicationContext;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,9 +9,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class IndexController {
 	
+	@Autowired
+	private ServletWebServerApplicationContext server;
+	
 	@GetMapping
 	public ResponseEntity<String> index() {
-		return ResponseEntity.ok("Hello World");
+		return ResponseEntity.ok("Hello World from port: " + server.getWebServer().getPort());
 	}
 	
 }

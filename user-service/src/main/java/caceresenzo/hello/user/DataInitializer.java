b37cd67989d19072da4fc6170989ec15d0f4f312
Caceresenzo <caceresenzo1502@gmail.com>
2022-08-06 22:07:02 +0200

    feat(user): making sure the environement is fetched

--- user-service/src/main/java/caceresenzo/hello/user/DataInitializer.java
+++ user-service/src/main/java/caceresenzo/hello/user/DataInitializer.java
@@ -5,6 +5,7 @@ import java.util.UUID;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
 import lombok.RequiredArgsConstructor;
@@ -13,11 +14,15 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class DataInitializer implements ApplicationRunner {
 	
+	private final Environment environment;
 	private final UserRepository userRepository;
 	private final RabbitTemplate rabbitTemplate;
 	
 	@Override
 	public void run(ApplicationArguments args) throws Exception {
+		System.out.println(environment.getRequiredProperty("spring.data.mongodb.database"));
+		System.out.println(environment.getRequiredProperty("spring.data.mongodb.host"));
+		
 		User user = userRepository.findByName("Enzo")
 			.orElseGet(() -> userRepository.save(new User()
 				.setId(UUID.randomUUID())
